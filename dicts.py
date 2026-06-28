00010d69c7c84ffcd705e6d141ce4b2eaeac3d8a
Munyaradzi Mandiwanzira <munyaradzimandiwanzira@Munyaradzis-MacBook-Air.local>
2026-06-28 13:34:21 -0400

    added notes

---
+++ dicts.py
@@ -0,0 +1,30 @@
+"""
+student = {}
+
+student["course"] = "Python"
+
+print(student) #--------------------> {'course': 'Python'}
+
+"""
+"""
+student ={
+    "name": "John",
+    "age": "22",
+    "city": "Hamilton"
+}
+
+student["city"] = "Toronto"
+
+print(student)
+
+"""
+
+student ={
+    "name": "John",
+    "age": "22",
+    "city": "Hamilton"
+}
+
+print(student.values())
+
+
