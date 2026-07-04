38f31e263e24fe9addfe590bbb3d85e568f7d5bc
Munyaradzi Mandiwanzira <munyaradzimandiwanzira@Munyaradzis-MacBook-Air.local>
2026-07-04 13:08:19 -0400

    homework commit

--- try_except.py
+++ try_except.py
@@ -1,6 +1,55 @@
+"""
 try:
     age = int(input("Enter age:"))
-    print(age)
+    
 
 except:
     print("Wakupenga here? Please enter a valid number.")
+
+"""
+"""
+try:
+    num1 = int(input("Number 1: "))
+    num2 = int(input("Number 2: "))
+
+    print(num1/num2)
+
+except ValueError:
+    print("Please enter valid numbers")
+
+except ZeroDivisionError:
+    print("Cannot divide by zero")
+
+else:
+    print("Division successful")
+
+"""
+"""
+try:
+     file = open("teams.txt")
+
+except FileNotFoundError:
+    print("File missing")
+
+finally:
+    print("Program Complete")
+
+"""
+'''
+try:
+    number = int(input())
+
+except:
+
+    print("Error")
+
+finally:
+
+    print("Finished")
+'''
+try:
+    age =int(input())
+
+except ValueError as error:
+    print(error)
+
