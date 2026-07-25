b76403da003c9dd4adc16a7cb3c3ea6b9f517e4a
Nathanielkm <nathanielmuzengeza@gmail.com>
2026-07-25 17:42:06 +0200

    Assignment

--- Sub_Sudoko.py
+++ Sub_Sudoko.py
@@ -2,27 +2,32 @@ size =int(input("Enter size of the grid: "))
 grid =[]
 
 for i in range(size):
-    row = input(f"Enter row {1 + i}: "). split()
-    row =[int(num) for num in row]
+    row = input(f"Enter row {1 + i}: ").split()
+    row = [int(num) for num in row]
     grid.append(row)
 
 def validateSudoku(grid):
     size = len(grid)
-    correct = set(range(1, size +1))
+    correct = set(range(1, size + 1))
     
     for row in grid:
-       if set(row) != correct:
-           return False
+        if set(row) != correct:
+            return False
        
     for col in range(size):
         column = []
         
         for row in grid:
-            column.append(row[column])
+            column.append(row[col])
             
         if set(column) != correct:
             return False
         
-        
-    print(grid)    
     return True
+
+if validateSudoku(grid):
+    print("Grid is valid:")
+    for row in grid:
+        print(row)
+else:
+    print("Grid is incorrect.")
